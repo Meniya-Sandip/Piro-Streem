cc429f85d91aabe299360c475ec2f81e856aec4f
Meniya Sandip <meniyasandip2006@gmail.com>
2024-12-01 23:09:20 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -14,7 +14,7 @@ class Var(object):
     name = str(getenv('name', 'THHFTOLBOT'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '0'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1002416380167"))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1002376421685"))
     PORT = int(getenv('PORT', 80))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -34,6 +34,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://razibot:razibot@cluster0.daqud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://jisshukh:jisshukh@cluster0.y9bck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "The_Happy_Hours"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "0")).split())) 
