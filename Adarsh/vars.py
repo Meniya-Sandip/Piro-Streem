9073919b8ef798d9c664135e6c6dfd8105cb88b7
Meniya Sandip <meniyasandip2006@gmail.com>
2024-12-01 22:58:31 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -10,18 +10,18 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', "18029060"))
     API_HASH = str(getenv('API_HASH', "c7e952440251e33bb5cce566b29f7254"))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', "8183356512:AAHI4kbADehs-P8IvCyCCzz9OVhwNWkmU0k"))
-    name = str(getenv('name', 'Dark_Technology_Bot'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', "7989012469:AAHAhqvM7kWTlorL_V8DgHqsWqsj-O74rZY"))
+    name = str(getenv('name', 'THHFTOLBOT'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '0'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1001992201683"))
-    PORT = int(getenv('PORT', 8080))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1002416380167"))
+    PORT = int(getenv('PORT', 80))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1277771711").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1991522624").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = str(getenv('APP_NAME'))
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', "Ma"))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', "SANDIP_1507"))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
